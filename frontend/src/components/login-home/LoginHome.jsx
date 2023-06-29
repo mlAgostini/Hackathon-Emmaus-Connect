e6c0604e4d91b102e4f2e9848d6dd886aad5dbe6
TessaFondeur <fondeurtessa.plume@gmail.com>
2023-06-29 10:30:22 +0200

    commit with CSS not finished

--- frontend/src/components/login-home/LoginHome.jsx
+++ frontend/src/components/login-home/LoginHome.jsx
@@ -2,31 +2,30 @@ import "./LoginHome.scss";
 
 export default function LoginHome() {
   return (
-    <>
-      <div className="container-login">
-        <div className="title-connexion">
-          <h1>Se Connecter</h1>
-        </div>
-        <div className="input-connexion">
-          <form className="login-form">
-            <div className="input-identification">
-              <input
-                type="text"
-                placeholder="exemple : tessafondeur ..."
-                size={40}
-              />
-            </div>
-            <div className="input-password">
-              <input
-                type="text"
-                placeholder="exemple : Pipou54 ..."
-                size={40}
-              />
-            </div>
-          </form>
-        </div>
+    <div className="container-login">
+      <div className="title-connexion">
+        <h1>SE CONNECTER</h1>
       </div>
-      <button type="button">Valider</button>
-    </>
+      <div className="input-connexion">
+        <form className="login-form">
+          <div className="input-identification">
+            <input
+              type="text"
+              placeholder="exemple : tessafondeur ..."
+              size={40}
+            />
+          </div>
+          <div className="input-password">
+            <input
+              type="password"
+              placeholder="exemple : Pipou54 ..."
+              size={40}
+            />
+          </div>
+
+          <button type="submit">Valider</button>
+        </form>
+      </div>
+    </div>
   );
 }
