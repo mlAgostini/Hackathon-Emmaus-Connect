e02e8ec4040b189d56206475cf63276b2b4b89aa
mlAgostini <valmont.vakarian@gmail.com>
2023-06-29 14:17:02 +0200

    Modify MySQL dump file

--- backend/database.sql
+++ backend/database.sql
@@ -16,6 +16,16 @@ CREATE TABLE `user` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'Tessa','Fondeur','Hauts-de-France','pokemon123'),(2,'Val','Vakarian','Occitanie','patacrepe'),(3,'Marie','VanGrevestruc','Grand-Est','redbull');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `model`
 --
 
@@ -36,6 +46,16 @@ CREATE TABLE `model` (
   CONSTRAINT `hgefihzqdq` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `model`
+--
+
+LOCK TABLES `model` WRITE;
+/*!40000 ALTER TABLE `model` DISABLE KEYS */;
+INSERT INTO `model` VALUES (1,'Samsung','Galaxy S10+','8','128','Bloqué','Rayé',1),(2,'Samsung','Galaxy Note 8','6','64','Reconditionné','RAS',2),(3,'Samsung','A40','4','64','Reparable','Ecran flou',3),(4,'Samsung','Galaxy S20','8','128','Comme neuf','RAS',1);
+/*!40000 ALTER TABLE `model` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `stock`
